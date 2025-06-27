d2ce6db66d89584712031bc0b99d97b8bd16ecfd
root <root@ip-172-31-47-41.ap-south-1.compute.internal>
2025-06-27 16:02:10 +0000

    appoi

--- appointment-service/index.js
+++ appointment-service/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/appointment', (req, res) => {
-  res.send('Appointment Service running!');
+app.get('/', (req, res) => {
+  res.send('✅ Appointment service is working!');
 });
 
 app.listen(port, () => {
-  console.log(`Appointment service running on port ${port}`);
+  console.log(`✅ Appointment service running on port ${port}`);
 });
 
