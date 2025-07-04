23923e16dcf7eae4644918df585102000eca70c1
root <root@Bala-Jarvis.localdomain>
2025-07-04 14:16:14 +0530

    Trigger redeploy - ECS should work now

--- patient-service/index.js
+++ patient-service/index.js
@@ -22,3 +22,4 @@ app.listen(port, host, () => {
 // trigger deploy Fri Jul  4 12:51:20 IST 2025
 // deploy trigger at Fri Jul  4 13:48:54 IST 2025
 // 🚀 Triggering GitHub Action at Fri Jul  4 14:06:13 IST 2025
+// trigger redeploy Fri Jul  4 14:16:14 IST 2025
